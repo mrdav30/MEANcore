61fac084db317cd30af7229daad787e4af94e793
david oravsky <david.oravsky@gmail.com>
2019-02-20 22:51:41 -0500

    Remove direct access guard

--- client/features/users/sign-in/sign-in.routes.ts
+++ client/features/users/sign-in/sign-in.routes.ts
@@ -2,12 +2,9 @@ import { Route } from '@angular/router';
 
 import { SignInComponent } from './sign-in.component';
 
-import { DirectAccessGuard } from '../../utils';
-
 export const SignInRoutes: Route[] = [
   {
     path: 'sign-in',
-    component: SignInComponent,
-    canActivate: [DirectAccessGuard]
+    component: SignInComponent
   }
 ];
--- client/features/users/sign-up/sign-up.routes.ts
+++ client/features/users/sign-up/sign-up.routes.ts
@@ -2,13 +2,10 @@ import { Route } from '@angular/router';
 
 import { SignUpComponent } from './sign-up.component';
 
-import { DirectAccessGuard } from '../../utils';
-
 export const SignUpRoutes: Route[] = [
   {
     path: 'sign-up',
-    component: SignUpComponent,
-    canActivate: [DirectAccessGuard]
+    component: SignUpComponent
   },
   {
     path: 'sign-up-admin/:install',
