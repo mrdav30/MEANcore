71b1b815b507acad11aa6eb37c51842048c28639
david oravsky <david.oravsky@gmail.com>
2019-02-23 23:39:35 -0500

    Update passport strategy to only return information required for auth

--- server/users/config/strategies/basic.js
+++ server/users/config/strategies/basic.js
@@ -4,35 +4,38 @@
  * Module dependencies.
  */
 var passport = require('passport'),
-	BasicStrategy = require('passport-http').BasicStrategy,
-	User = require('mongoose').model('User');
+  BasicStrategy = require('passport-http').BasicStrategy,
+  User = require('mongoose').model('User');
 
-module.exports = function() {
-	// Use basic strategy
-	passport.use(new BasicStrategy({
-			usernameField: 'username',
-			passwordField: 'password'
-		},
-		function(username, password, done) {
-			User.findOne({
-				username: username
-			}, function(err, user) {
-				if (err) {
-					return done(err);
-				}
-				if (!user) {
-					return done(null, false, {
-						message: 'Unknown user or invalid password'
-					});
-				}
-				if (!user.authenticate(password)) {
-					return done(null, false, {
-						message: 'Unknown user or invalid password'
-					});
-				}
+module.exports = function () {
+  // Use basic strategy
+  // Retrieve required fields for auth only
+  passport.use(new BasicStrategy({
+      usernameField: 'username',
+      passwordField: 'password'
+    },
+    function (username, password, done) {
+      User.findOne({
+          username: username
+        })
+        .select('username displayName email roles knownIPAddresses password salt')
+        .exec(function (err, user) {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false, {
+              message: 'Unknown user or invalid password'
+            });
+          }
+          if (!user.authenticate(password)) {
+            return done(null, false, {
+              message: 'Unknown user or invalid password'
+            });
+          }
 
-				return done(null, user);
-			});
-		}
-	));
+          return done(null, user);
+        });
+    }
+  ));
 };
--- server/users/config/strategies/local.js
+++ server/users/config/strategies/local.js
@@ -4,33 +4,35 @@
  * Module dependencies.
  */
 var passport = require('passport'),
-	LocalStrategy = require('passport-local').Strategy,
-	User = require('mongoose').model('User');
+  LocalStrategy = require('passport-local').Strategy,
+  User = require('mongoose').model('User');
 
 module.exports = function () {
-	// Use local strategy
-	passport.use(new LocalStrategy({
-			usernameField: 'usernameOrEmail',
-			passwordField: 'password'
-		},
-		function (usernameOrEmail, password, done) {
-			User.findOne({
-				$or: [{
-					username: usernameOrEmail.toLowerCase()
-				  }, {
-					email: usernameOrEmail.toLowerCase()
-				  }]
-			}, function (err, user) {
-				if (err) {
-					return done(err);
-				} else if (!user || !user.authenticate(password)) {
-					return done(null, false, {
-						message: 'Invalid username or password (' + (new Date()).toLocaleTimeString() + ')'
-					});
-				}
+  // Use local strategy
+  passport.use(new LocalStrategy({
+      usernameField: 'usernameOrEmail',
+      passwordField: 'password'
+    },
+    function (usernameOrEmail, password, done) {
+      User.findOne({
+          $or: [{
+            username: usernameOrEmail.toLowerCase()
+          }, {
+            email: usernameOrEmail.toLowerCase()
+          }]
+        })
+        .select('username displayName email roles knownIPAddresses password salt')
+        .exec(function (err, user) {
+          if (err) {
+            return done(err);
+          } else if (!user || !user.authenticate(password)) {
+            return done(null, false, {
+              message: 'Invalid username or password (' + (new Date()).toLocaleTimeString() + ')'
+            });
+          }
 
-				return done(null, user);
-			});
-		}
-	));
+          return done(null, user);
+        });
+    }
+  ));
 };
