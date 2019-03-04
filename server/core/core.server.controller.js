c67d0c0c385938d33b2e22559c43b711d6d00119
ACMOORE\doravsky <doravsky@acmoore.com>
2019-03-04 14:57:47 -0500

    Add ssr with puppeteer

--- server/core/core.server.controller.js
+++ server/core/core.server.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var path = require('path');
+var path = require('path'),
+  config = require(path.resolve('./config/config')),
+  isBot = config.helpers.isBot,
+  ssrService = config.services.ssrService;
 
 /**
  * Get extention from path
@@ -22,19 +25,28 @@ exports.renderNotFound = renderNotFound;
 /**
  * Render the main application page
  */
-exports.renderIndex = function (req, res) {
+exports.renderIndex = async function (req, res) {
   var config = req.app.locals.config;
   var ext = getExtention(req.url);
   // TODO: change the way to identify extentions
   var exts = ['html', 'css', 'js', 'pdf', 'jpeg', 'gif', 'png'];
   if (!config.app.defaultPage || ~exts.indexOf(ext)) { // if not in extentions give 404
-    // next();
     renderNotFound(req, res); // We know its not found
   } else {
-    var rootDir = path.normalize(config.staticFiles);
-    res.sendFile(config.app.defaultPage, {
-      root: rootDir
-    });
+    if (!isBot(req.headers['user-agent'])) {
+      var rootDir = path.normalize(config.staticFiles);
+      res.sendFile(config.app.defaultPage, {
+        root: rootDir
+      });
+    } else {
+      const {
+        html,
+        ttRenderMs
+      } = await ssrService.ssr(`${req.protocol}://${req.get('host')}/index.html`);
+      // Add Server-Timing! See https://w3c.github.io/server-timing/.
+      res.set('Server-Timing', `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`);
+      return res.status(200).send(html); // Serve prerendered page as response.
+    };
   }
 };
 
--- server/services.js
+++ server/services.js
@@ -1,15 +1,13 @@
 var emailService = require('./services/email.service'),
-  // oracleQueryService = require('./services/oracle-query.service');
-  // mssqlQueryService = require('./services/mssql-query.service');
   splunkLogger = require('./services/splunk-logger.service'),
   googleAnalytics = require('./services/google-analytics.service'),
-  transferService = require('./services/transfer-file.service');
+  transferService = require('./services/transfer-file.service'),
+  ssrService = require('./services/ssr.service');
 
 module.exports = {
   emailService,
-  //oracleQueryService,
-  // mssqlQueryService,
   splunkLogger,
   googleAnalytics,
-  transferService
+  transferService,
+  ssrService
 };
