f799b2a2e222860bfda0f3c9c4f5d13c7bc30650
ACMOORE\doravsky <doravsky@acmoore.com>
2019-03-29 11:48:38 -0400

    Fix directory paths

--- scripts/generate-ssl-certs.sh
+++ scripts/generate-ssl-certs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ ! -e ../server.js ]
+if [ ! -e ./server.js ]
 then
 	echo "Error: could not find main application server.js file"
-	echo "You should run the generate-ssl-certs.sh script from the root MEANcore application script directory"
+	echo "You should run the generate-ssl-certs.sh script from the root MEANcore application directory"
 	echo "i.e: bash generate-ssl-certs.sh"
 	exit -1
 fi
 
 echo "Generating self-signed certificates..."
-mkdir -p ../config/sslcerts
-openssl genrsa -out ../config/sslcerts/key.pem 4096
-openssl req -new -key ../config/sslcerts/key.pem -out ../config/sslcerts/csr.pem
-openssl x509 -req -days 365 -in ../config/sslcerts/csr.pem -signkey ../config/sslcerts/key.pem -out ../config/sslcerts/cert.pem
-rm ../config/sslcerts/csr.pem
-chmod 600 ../config/sslcerts/key.pem ../config/sslcerts/cert.pem
+mkdir -p ./config/sslcerts
+openssl genrsa -out ./config/sslcerts/key.pem 4096
+openssl req -new -key ./config/sslcerts/key.pem -out ./config/sslcerts/csr.pem
+openssl x509 -req -days 365 -in ./config/sslcerts/csr.pem -signkey ./config/sslcerts/key.pem -out ./config/sslcerts/cert.pem
+rm ./config/sslcerts/csr.pem
+chmod 600 ./config/sslcerts/key.pem ./config/sslcerts/cert.pem
