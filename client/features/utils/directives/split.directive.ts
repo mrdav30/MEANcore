4bc81795441b6fb64db447a243063ad9c81f4b05
david oravsky <david.oravsky@gmail.com>
2020-02-02 21:03:50 -0500

    bugfix: Split directive not triggering invalid field

--- client/features/utils/directives/split.directive.ts
+++ client/features/utils/directives/split.directive.ts
@@ -1,64 +1,78 @@
-import { Directive, HostListener, forwardRef, Renderer2, ElementRef } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {
+  Directive,
+  HostListener,
+  forwardRef,
+  Renderer2,
+  ElementRef
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR
+} from '@angular/forms';
 
-import { chain, trim } from 'lodash';
+import {
+  chain,
+  trim
+} from 'lodash';
 
-const noop = () => { };  // tslint:disable-line
+const noop = () => {}; // tslint:disable-line
 
 const SPLIT_VALUE_ACCESSOR: any = {
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => SplitDirective),
-    multi: true
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => SplitDirective),
+  multi: true
 };
 
 @Directive({
-    selector: '[appSplitControl]',
-    providers: [SPLIT_VALUE_ACCESSOR]
+  selector: '[appSplitControl]',
+  providers: [SPLIT_VALUE_ACCESSOR]
 })
 export class SplitDirective implements ControlValueAccessor {
-    private onChange: (arg0: string[]) => void;
+  private onChange: (arg0: string[]) => void;
 
-    private onTouchedCallback: () => void = noop;
+  private onTouchedCallback: () => void = noop;
 
-    constructor(
-        private renderer: Renderer2,
-        private element: ElementRef
-    ) { }
+  constructor(
+    private renderer: Renderer2,
+    private element: ElementRef
+  ) {}
 
-    @HostListener('input', ['$event.target.value'])
-    input(value: any) {
-        if (value) {
-            this.onChange(
-                chain(value)
-                    .split(',')
-                    .filter((el) => {
-                        // filter out null values
-                        return el.length > 0;
-                    })
-                    .map((tag: string) => {
-                        // trim whitespace
-                        return trim(tag);
-                    })
-                    .value()
-            );
-        }
+  @HostListener('input', ['$event.target.value'])
+  input(value: any) {
+    if (value) {
+      this.onChange(
+        chain(value)
+        .split(',')
+        .filter((el) => {
+          // filter out null values
+          return el.length > 0;
+        })
+        .map((tag: string) => {
+          // trim whitespace
+          return trim(tag);
+        })
+        .value()
+      );
+    } else {
+      this.onChange(null);
     }
+  }
 
-    // From ControlValueAccessor interface
-    writeValue(value: any): void {
-        if (value) {
-            const element = this.element.nativeElement;
-            this.renderer.setProperty(element, 'value', value.join(','));
-        }
+  // From ControlValueAccessor interface
+  writeValue(value: any): void {
+    if (value) {
+      const element = this.element.nativeElement;
+      this.renderer.setProperty(element, 'value', value.join(','));
     }
+  }
 
-    // From ControlValueAccessor interface
-    registerOnChange(fn: any): void {
-        this.onChange = fn;
-    }
+  // From ControlValueAccessor interface
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
 
-    // From ControlValueAccessor interface
-    registerOnTouched(fn: any) {
-        this.onTouchedCallback = fn;
-    }
+  // From ControlValueAccessor interface
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
 }
