38f88ee11322f0d0e3e0838afdeddb6f28871d16
david oravsky <david.oravsky@gmail.com>
2021-03-27 19:01:01 -0400

    update: pre & post commit hooks to capture additional changes to package link

--- tools/unbundle-packages.js
+++ tools/unbundle-packages.js
@@ -1,28 +1,28 @@
- import fse from 'fs-extra';
- import _ from 'lodash';
- import chalk from 'chalk';
+ import fs from 'fs';
  import bundleConfig from './config.init.js';
 
  // Revert to backup (i.e. core.package.json/mod.package.json)
  const revertToBackupPkg = async () => {
-   console.log(chalk.cyan('Unbundling linked package.json.'));
+   console.log('Unbundling linked package.json.');
 
-   if (fse.existsSync(bundleConfig.CORE_PKG_BKP)) {
+   if (fs.existsSync(bundleConfig.CORE_PKG_BKP)) {
      // Overwrite current core package.json with back up!
-     fse.copyFileSync(bundleConfig.CORE_PKG_BKP, bundleConfig.LINK_CORE_PKG);
+     fs.copyFileSync(bundleConfig.CORE_PKG_BKP, bundleConfig.LINK_CORE_PKG);
    }
 
-   _.forEach(bundleConfig.ALL_MODULES, (mod) => {
-     if (fse.existsSync(mod.LINK_MOD_PKG)) {
+   bundleConfig.ALL_MODULES.forEach((mod) => {
+     if (fs.existsSync(mod.LINK_MOD_PKG)) {
        // Overwrite module package.json with back up!
-       fse.copyFileSync(mod.LINK_MOD_PKG, mod.MOD_PKG_BKP);
+       fs.copyFileSync(mod.LINK_MOD_PKG, mod.MOD_PKG_BKP);
      } else {
        // Back up current module package.json
-       fse.copyFileSync(mod.MOD_PKG_BKP, mod.LINK_MOD_PKG);
+       fs.copyFileSync(mod.MOD_PKG_BKP, mod.LINK_MOD_PKG);
      }
    });
 
-   console.log(chalk.cyan('Unbundling linked package.json finished.'));
+   console.log('Unbundling linked package.json finished.');
+
+   return 1;
  }
 
  bundleConfig.init(() => {
